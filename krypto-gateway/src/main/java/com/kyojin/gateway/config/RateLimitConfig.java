4428094f229b8be1ee61beea9836077f31e4b280
tahajaiti <taha.jaiti@gmail.com>
2025-12-22 16:15:28 +0100

    FEAT: KP-5 add rate limiting, logging filter and exception handling to the api gateway #done

---
+++ krypto-gateway/src/main/java/com/kyojin/gateway/config/RateLimitConfig.java
@@ -0,0 +1,22 @@
+package com.kyojin.gateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+@Configuration
+public class RateLimitConfig {
+
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange -> Mono.just(
+                Objects.requireNonNull(exchange.getRequest()
+                                .getRemoteAddress())
+                        .getAddress()
+                        .getHostAddress()
+        );
+    }
+}
