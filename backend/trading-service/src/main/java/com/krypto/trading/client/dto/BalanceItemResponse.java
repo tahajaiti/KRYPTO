2f362b9b38aa2fc1306568af2d51e2d6151f1aa1
tahajaiti <taha.jaiti@gmail.com>
2026-03-30 09:12:11 +0100

    feat(trading): add BalanceItemResponse [KP-7]

---
+++ backend/trading-service/src/main/java/com/krypto/trading/client/dto/BalanceItemResponse.java
@@ -0,0 +1,19 @@
+package com.krypto.trading.client.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BalanceItemResponse {
+    private UUID coinId;
+    private String symbol;
+    private BigDecimal balance;
+}
