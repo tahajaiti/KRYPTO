8dda27e8a158bf13ae32be4e7ea596a90ea2ad73
tahajaiti <taha.jaiti@gmail.com>
2026-03-30 09:12:05 +0100

    fix(blockchain): update build.gradle [KP-1]

--- backend/blockchain-service/build.gradle.kts
+++ backend/blockchain-service/build.gradle.kts
@@ -1,13 +1,14 @@
 dependencies {
     implementation(project(":common"))
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.data:spring-data-commons")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-amqp")
     implementation("org.springframework.retry:spring-retry")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    runtimeOnly("org.postgresql:postgresql")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.amqp:spring-rabbit-test")
